0551258fcccd778e3126a27e8b246b8ff835f458
Sam Harper <sam.j.harper@gmail.com>
2017-01-07 13:58:37 +0000

    updated example

--- VID/test/heepV70PATExample_cfg.py
+++ VID/test/heepV70PATExample_cfg.py
@@ -19,45 +19,22 @@ from Configuration.AlCa.autoCond import autoCond
 process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_2016_TrancheIV_v4', '') #
 
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring(
         '/store/mc/RunIISpring16MiniAODv2/ZToEE_NNPDF30_13TeV-powheg_M_200_400/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/20000/0C242E56-BA3A-E611-9B2D-0242AC130004.root',
         )
 )
 
-#setup the VID with HEEP 7.0
-from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
-# turn on VID producer, indicate data format  to be
-# DataFormat.AOD or DataFormat.MiniAOD, as appropriate
-switchOnVIDElectronIdProducer(process, DataFormat.MiniAOD)
-
-process.load("HEEP.VID.addHEEPV70ToEles_cfi") 
-
-# define which IDs we want to produce
-my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV70_cff']
-#add them to the VID producer
-for idmod in my_id_modules:
-    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
-process.heepIDVarValueMaps.elesMiniAOD = cms.InputTag("slimmedElectrons",processName=cms.InputTag.skipCurrentProcess())
-process.heepIDVarValueMaps.elesMiniAOD = cms.InputTag("slimmedElectrons",processName=cms.InputTag.skipCurrentProcess())
-process.egmGsfElectronIDs.physicsObjectSrc = \
-    cms.InputTag("slimmedElectrons",processName=cms.InputTag.skipCurrentProcess())
-#process.electronMVAValueMapProducer.src = \
-#    cms.InputTag("slimmedElectrons",processName=cms.InputTag.skipCurrentProcess())
-#process.electronRegressionValueMapProducer.src = \
-#        cms.InputTag("slimmedElectrons",processName=cms.InputTag.skipCurrentProcess())
-
+from HEEP.VID.tools import addHEEPV70ElesMiniAOD
+addHEEPV70ElesMiniAOD(process,useStdName=True)
 
 #this is our example analysis module reading the results
 process.heepIdExample = cms.EDAnalyzer("HEEPV70PATExample",
                                        eles=cms.InputTag("slimmedElectrons"),
-                                       trkIsolMap=cms.InputTag("heepIDVarValueMaps","eleTrkPtIso"),
-                                       vid=cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV70")
                                        )
 
 process.p = cms.Path(
-    process.egmGsfElectronIDSequence* 
-    process.heepIDApplication*
+    process.heepSequence*
     process.heepIdExample) #our analysing example module, replace with your module
 
 #dumps the products made for easier debugging
