73ea51c8932f8c6d53a6b0e65f9d608146ee9bd9
Sam Harper <sam.j.harper@gmail.com>
2018-08-21 15:29:06 +0100

    adding CutBasedID

--- VID/interface/CutNrs.h
+++ VID/interface/CutNrs.h
@@ -5,12 +5,31 @@ namespace cutnrs {
   class HEEPV70 {
   public:
     enum CutIndex {
-      ET=0,ETA,DETAINSEED,DPHIIN,SIGMAIETAIETA,E2X5OVER5X5,HADEM,TRKISO,EMHADD1ISO,DXY,MISSHITS,ECALDRIVEN
+      ET=0,ETA=1,DETAINSEED=2,DPHIIN=3,SIGMAIETAIETA=4,E2X5OVER5X5=5,HADEM=6,TRKISO=7,EMHADD1ISO=8,DXY=9,MISSHITS=10,ECALDRIVEN=11
     };
     
     constexpr static unsigned int kMaxBitNr=ECALDRIVEN;
     constexpr static unsigned int kFullMask=( 0x1 << (kMaxBitNr+1) ) -1;
   };
+  class CutSum16V1 {
+  public:
+    enum CutIndex {
+      ET=0,ETA=1,DETAINSEED=2,DPHIIN=3,SIGMAIETAIETA=4,HADEM=5,INVEINVP=6,PFISO=7,CONVETO=8,MISSHITS=9
+    };
+    constexpr static unsigned int kMaxBitNr=MISSHITS;
+    constexpr static unsigned int kFullMask=( 0x1 << (kMaxBitNr+1) ) -1;
+  };
+  
+  class CutFall17V1 {
+  public:
+    enum CutIndex {
+      ET=0,ETA=1,DETAINSEED=2,DPHIIN=3,SIGMAIETAIETA=4,HADEM=5,INVEINVP=6,PFISO=7,CONVETO=8,MISSHITS=9
+    };
+    constexpr static unsigned int kMaxBitNr=MISSHITS;
+    constexpr static unsigned int kFullMask=( 0x1 << (kMaxBitNr+1) ) -1;
+  };
+  
+
 }
 
 #endif
